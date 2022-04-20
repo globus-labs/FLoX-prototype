abda1f0147f864ba017167e95514b6fb307883c6
Nikita Kotsehub <47567310+nikita-kotsehub@users.noreply.github.com>
2022-04-20 22:16:57 +0200

    setup update

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Pyth   on :: 3.11'
+        'Programming Language :: Python :: 3.11'
   ],
 )
