570822bbff860cc7aa552477b8539e67d3ff10b4
Nikita Kotsehub <47567310+nikita-kotsehub@users.noreply.github.com>
2022-10-27 17:34:55 -0300

    updated README from .md to .rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FLoX'
+copyright = '2022, Globus Labs'
+author = 'Globus Labs'
 
 release = '0.1'
 version = '0.1.0'
